b475b8fc22b60456ee6f6c61d7301928a2adcd07
Archange Mbah Tchategouang <archange.mbah.tchategouang@mni.thm.de>
2024-07-09 11:25:01 +0200

    complete

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 
 function getInputValue(){
+    /** we get the elements */
     var inputElement=document.getElementById('input');
     var item=document.createElement('div');
-    item.innerHTML=inputElement.value;
+     item.innerHTML=inputElement.value;
+    var deleteB=document.createElement('button');
+    deleteB.textContent='delete me';
+    deleteB.className='delete';
+    deleteB.onclick=function(){
+        item.remove();
+        deleteB.remove();
+    }
+    item.appendChild(deleteB);
     document.getElementById('bottom').appendChild(item);
     inputElement.value='';
 } 
-function deleteElement(){
-   
-    
-}
+
 
